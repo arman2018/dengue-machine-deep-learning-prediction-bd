b5b11f58a75dddb432053971c7274a0cf253e1cc
Arman Hossain Chowdhury <96700045+arman2018@users.noreply.github.com>
2025-03-18 19:33:09 +0600

    Update Descriptive statistics.R

--- Descriptive statistics.R
+++ Descriptive statistics.R
@@ -7,7 +7,9 @@ library(readxl)
 library(dplyr)
 library(tidyr)
 library(car)
-
+library(ggplot2)
+library(lubridate)
+library(gridExtra)
 
 #Upload the data
 data<-read_excel("data_DENV.xlsx",sheet="data")
@@ -55,16 +57,40 @@ desc_stats <- df_long %>%
 print(desc_stats)
 View(desc_stats)
 
-##Monthly distribution precipitation in BD
+##Monthly distribution of climate patterns in BD
 data$Months <- month(data$Months, label = TRUE)  # Label = TRUE gives month names
 
 # Plot the monthly boxplot
-ggplot(data, aes(x = Months, y = x5)) +
+p1<-ggplot(data, aes(x = Months, y = x1)) +
   geom_boxplot(fill = "lightblue", color = "black") +
-  labs(title = "Monthly Distribution of Precipitation (2000-2022)",
-       x = "Months", y = "Precipitation (mm)") +
+  labs(title = "A: Mean temperature (C)",
+       x = "", y = "Number") +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1));p1
+
+p2<-ggplot(data, aes(x = Months, y = x4)) +
+  geom_boxplot(fill = "firebrick", color = "black") +
+  labs(title = "B: Relative humidity (%)",
+       x = "", y = "") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1));p2
+
+p3<-ggplot(data, aes(x = Months, y = x5)) +
+  geom_boxplot(fill = "seagreen", color = "black") +
+  labs(title = "C: Precipitation (mm)",
+       x = "Months", y = "Number") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1));p3
+
+p4<-ggplot(data, aes(x = Months, y = x7)) +
+  geom_boxplot(fill = "darkorange", color = "black") +
+  labs(title = "D: Maximum wind speed (m/s)",
+       x = "Months", y = "") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1));p4
+
+##combine the plots
+grid.arrange(p1,p2,p3,p4,ncol=2)
 
 ##Multicollinearity check
 model_linear<-lm(y~x1+x2+x3+x4+x5+x6+x7,data=data)
